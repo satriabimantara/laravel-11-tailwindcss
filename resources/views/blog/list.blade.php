86c36ea61d14ddf95510d7a9afa1fc117ac3a588
I Made Satria Bimantara <satriabimantara.md@gmail.com>
2024-06-09 11:34:49 +0800

    - Installing TailwindCSS and Alpine.JS into root project
    - Creating web page UI using Tailwind template
    
    - Configuring some tailwind utility in our home.blade.php

---
+++ resources/views/blog/list.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Page</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to Blog Page</h1>
+    </header>
+    <article>
+        <h4>Judul Artikel 1</h4>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In cupiditate atque ad incidunt delectus voluptatem voluptatum animi esse maxime quis adipisci, odio iste natus est provident quidem quod minus ratione.
+
+        </p>
+    </article>
+    <article>
+        <h4>Judul Artikel 1</h4>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In cupiditate atque ad incidunt delectus voluptatem voluptatum animi esse maxime quis adipisci, odio iste natus est provident quidem quod minus ratione.
+
+        </p>
+    </article>
+</body>
+</html>
